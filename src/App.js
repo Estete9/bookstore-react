c83306cc2f80c24f07a1c7a0add4a053d3682cb5
Esteban <cepalacios@outlook.com>
2023-09-28 22:34:51 -0500

    Remove placeholder files and refactor

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      this is App
+      <header className="App-header">this is the header</header>
     </div>
   );
 }
