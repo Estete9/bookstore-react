8c93ab2bcc1c950473a8530a00793764c9d9d7dc
Esteban <cepalacios@outlook.com>
2023-09-29 11:18:10 -0500

    Implement Header component

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import styles from '../styles/header.module.css';
+
+function Header() {
+  const links = [
+    { path: '/', text: 'BOOKS' },
+    { path: 'categories', text: 'CATEGORIES' },
+  ];
+
+  return (
+    <div className={styles.Header}>
+      <a className={styles.logo} href="/">
+        Bookstore CMS
+      </a>
+      <ul>
+        {links.map((navItem) => (
+          <li key={navItem.text.toLowerCase()} className={styles.navbarItem}>
+            {navItem.text}
+          </li>
+        ))}
+      </ul>
+      <a href="/">profile</a>
+    </div>
+  );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>,
 );
