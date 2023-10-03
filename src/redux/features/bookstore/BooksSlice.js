03e0b3f87aa70e5c092503bc7a07a05b10bc2b0b
Esteban <cepalacios@outlook.com>
2023-10-03 12:41:25 -0500

    Add hardcoded list of books

--- src/redux/features/bookstore/BooksSlice.js
+++ src/redux/features/bookstore/BooksSlice.js
@@ -1,7 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  books: [],
+  books: [
+    {
+      item_id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
 };
 
 const BooksSlice = createSlice({
@@ -9,6 +28,7 @@ const BooksSlice = createSlice({
   initialState,
   reducers: {
     addBook: (store, action) => {
+      console.log(`payload ${action.payload}`);
       const { id, title, author } = action.payload;
       const newBook = {
         id,
